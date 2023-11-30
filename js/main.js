8c6bb217eb1f77c049c4cac520a6a89e74630982
veronicasorge <150162661+veronicasorge@users.noreply.github.com>
2023-12-01 00:13:57 +0100

    detail page styling

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function changeImage() {
+  var cartImage = document.getElementById("cart1");
+  if (cartImage.src.match("photos/bag.png")) {
+    cartImage.src = "photos/bag-with-item.png";
+  } else {
+    cartImage.src = "photos/bag.png";
+    cartImage.classList.add("changed-image");
+  }
+}
+
+function updateImages() {
+  const dropdown = document.getElementById("dropdown");
+  const mainProductPicture = document.getElementById("main-product-picture");
+  const detailPicture3 = document.getElementById("detail-picture-3");
+
+  if (dropdown.value === "option2") {
+    mainProductPicture.src = "photos/detail-3.jpg";
+    detailPicture3.src = "photos/main-rainbow-earrings.jpg";
+  } else {
+    mainProductPicture.src = "photos/main-rainbow-earrings.jpg";
+    detailPicture3.src = "photos/detail-3.jpg";
+  }
+}
--- main.js
+++
@@ -1,9 +0,0 @@
-function changeImage() {
-  var cartImage = document.getElementById("cart1");
-  if (cartImage.src.match("photos/bag.png")) {
-    cartImage.src = "photos/bag-with-item.png";
-  } else {
-    cartImage.src = "photos/bag.png";
-    cartImage.classList.add("changed-image");
-  }
-}
